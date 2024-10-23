455009dfbdd31deb8b70a343c1b9dc8c07aef4af
pedrodruviaro <pedrodruviaro@gmail.com>
2024-10-23 19:43:42 -0300

    feat: render idea layout based on user login state

---
+++ components/Public/Header/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="py-3 shadow-sm">
+    <BaseContainer class="flex items-center justify-between gap-3 flex-wrap">
+      <NuxtLink to="/app" class="max-w-28 block">
+        <div>
+          <Logo variant="dark" />
+        </div>
+      </NuxtLink>
+
+      <div class="flex items-center gap-2">
+        <Button
+          as="router-link"
+          to="/auth/login"
+          label="Comece a criar"
+          outlined
+        />
+      </div>
+    </BaseContainer>
+  </header>
+</template>
--- layouts/idea.vue
+++ layouts/idea.vue
@@ -1,18 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const router = useRouter()
+
+const user = useSupabaseUser()
+const session = useSupabaseSession()
+
+const isLogged = computed(() => {
+  return Boolean(user.value && session.value)
+})
+</script>
 
 <template>
-  <main class="bg-pink-300 h-full min-h-screen grid relative">
-    <BaseContainer class="py-10 relative z-10">
-      <slot />
-    </BaseContainer>
-
-    <div class="absolute inset-0 w-full h-full border">
-      <img
-        src="/public/images/sky-pattern.png"
-        alt=""
-        aria-hidden="true"
-        class="w-full h-full object-cover"
+  <div>
+    <div class="relative z-10">
+      <AdminHeader
+        v-if="isLogged"
+        @wants-create-idea="router.push('/app/idea/new')"
       />
+      <PublicHeader v-else />
     </div>
-  </main>
+
+    <main class="bg-pink-300 h-full min-h-screen grid relative">
+      <BaseContainer class="py-10 relative z-10">
+        <slot />
+      </BaseContainer>
+
+      <div class="absolute inset-0 w-full h-full border">
+        <img
+          src="/public/images/sky-pattern.png"
+          alt=""
+          aria-hidden="true"
+          class="w-full h-full object-cover"
+        />
+      </div>
+    </main>
+  </div>
 </template>
